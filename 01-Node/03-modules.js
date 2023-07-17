a4f4a345e0f669375c9171878dc797f3dd549051
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-07-17 13:10:10 +0500

    Modultes Setup

---
+++ 01-Node/03-modules.js
@@ -0,0 +1,12 @@
+// Modules
+
+const fakher = "fakher";
+const hammad = "hammad";
+
+const sayHi = (name) => {
+    console.log(`Hello there ${name}`);
+}
+
+sayHi("shaban");
+sayHi(fakher);
+sayHi(hammad);
