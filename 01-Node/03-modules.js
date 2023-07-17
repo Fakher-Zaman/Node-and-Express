93fe02c57c2ca65e6323e8d5789d565efeb79e45
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-07-17 17:11:24 +0500

    Mind Grenade

--- 01-Node/03-modules.js
+++ 01-Node/03-modules.js
@@ -2,10 +2,9 @@
 // Modules - Encapsulated Code (only share minimum)
 
 const names = require("./04-names");
-// console.log(names);
 const sayHi = require("./05-utils");
 const data = require("./06-alternative-flavor");
-console.log(data);  // Returns an object
+require("./07-mind-grenade");
 
 sayHi("shaban");
 sayHi(names.fakher);
