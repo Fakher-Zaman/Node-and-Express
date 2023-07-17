de17a2837931f3885134b535edb2a110c1b17eb2
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-07-17 13:51:57 +0500

    Alternative Syntax

--- 01-Node/03-modules.js
+++ 01-Node/03-modules.js
@@ -2,8 +2,10 @@
 // Modules - Encapsulated Code (only share minimum)
 
 const names = require("./04-names");
-const sayHi = require("./05-utils");
 // console.log(names);
+const sayHi = require("./05-utils");
+const data = require("./06-alternative-flavor");
+console.log(data);  // Returns an object
 
 sayHi("shaban");
 sayHi(names.fakher);
---
+++ 01-Node/06-alternative-flavor.js
@@ -0,0 +1,8 @@
+// Array
+module.exports.items = ["item1", "item2"];
+
+// Object
+const person = {
+    name: "fakher",
+}
+module.exports.singlePerson = person;
