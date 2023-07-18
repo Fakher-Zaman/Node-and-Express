15463a64aa06b52eed840dedfa31ecfdf629fc75
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-07-18 20:16:02 +0500

    Fs Module (Sync)

---
+++ 01-Node/10-fs-sync.js
@@ -0,0 +1,11 @@
+const { readFileSync, writeFileSync } = require('fs');
+
+const first = readFileSync('./content/first.txt', 'utf8');
+const second = readFileSync('./content/second.txt', 'utf8');
+console.log(first, second);
+
+writeFileSync(
+    './content/result-sync.txt',
+    `Here is the result: ${first}, ${second}`,
+    { flag: 'a' }
+);
