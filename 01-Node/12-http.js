35b549262be42a9efc94c7aba6586cbbe766a07e
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-07-19 11:53:16 +0500

    Http Module (More Features)

--- 01-Node/12-http.js
+++ 01-Node/12-http.js
@@ -1,8 +1,16 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.write('welcome to our home page');
-    res.end();
+    if (req.url === '/') {
+        res.end('welcome to our home page');
+    } else if (req.url === '/about') {
+        res.end('here is our short history');
+    } else {
+        res.end(`
+        <h1>Oops!</h1>
+        <p>We can't seem to find the page you are looking for</p>
+        <a href='/'>back home</a>`);
+    }
 });
 
 server.listen(5000);
