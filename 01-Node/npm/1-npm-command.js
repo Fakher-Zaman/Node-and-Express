1b8021527e45435f5c2ed0956f7a87e9ad63142b
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-07-19 12:25:53 +0500

    NPM Command

---
+++ 01-Node/npm/1-npm-command.js
@@ -0,0 +1,9 @@
+// npm - global command, comes with node
+// npm --version
+
+// local dependency - use it only in this particular project
+// npm i <packageNode>
+
+// global dependency - use it in any project
+// npm install -g <packageName>
+// sudo npm install -g <packageName> (mac)
