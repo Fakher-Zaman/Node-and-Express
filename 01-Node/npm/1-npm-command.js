34280c3e3d3824263e5be7c107089b7913f8063d
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-07-19 12:53:15 +0500

    First Package

--- 01-Node/npm/1-npm-command.js
+++ 01-Node/npm/1-npm-command.js
@@ -7,3 +7,14 @@
 // global dependency - use it in any project
 // npm install -g <packageName>
 // sudo npm install -g <packageName> (mac)
+
+// package.json - manifest file (stores important info about project/package)
+// manual approach (create package.json in the root, create properties etc)
+// npm init (step by step, press enter to skip)
+// npm init -y (everything default) 
+
+const _ = require('lodash');
+
+const items = [1, [2, [3, [4]]]];
+const newItems = _.flattenDeep(items);
+console.log(newItems);
