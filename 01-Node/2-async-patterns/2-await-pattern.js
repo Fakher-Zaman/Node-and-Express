a4a2d15a2fdb2b0e31d4081012a12b05f3faa020
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-07-20 17:08:14 +0500

    Async Patterns - Node's Native Option 1

--- 01-Node/2-async-patterns/2-await-pattern.js
+++ 01-Node/2-async-patterns/2-await-pattern.js
@@ -1,27 +1,14 @@
-const { readFile } = require('fs');
-
-const getText = (path) => {
-    return new Promise((resolve, reject) => {
-        readFile(path, 'utf8', (err, result) => {
-            if (err) {
-                reject(err);
-            }
-            else {
-                resolve(result);
-            }
-        });
-    });
-}
-
-// getText('../content/first.txt')
-//     .then((result) => console.log(result))
-//     .catch((err) => console.log(err));
+const { readFile, writeFile } = require('fs');
+const util = require('util');
+const readFilePromise = util.promisify(readFile);
+const writeFilePromise = util.promisify(writeFile);
 
 // ==>  async/await
 const start = async () => {
     try {
-        const first = await getText('../content/first.txt');
-        const second = await getText('../content/second.txt');
+        const first = await readFilePromise('../content/first.txt', 'utf8');
+        const second = await readFilePromise('../content/second.txt', 'utf-8');
+        await writeFilePromise('../content/result-mind-grenade.txt', `THIS IS AWESOME : ${first} ${second}`);
         console.log(first, second);
     } catch (error) {
         console.log(error);
@@ -29,3 +16,20 @@ const start = async () => {
 }
 
 start();
+
+// const getText = (path) => {
+//     return new Promise((resolve, reject) => {
+//         readFile(path, 'utf8', (err, result) => {
+//             if (err) {
+//                 reject(err);
+//             }
+//             else {
+//                 resolve(result);
+//             }
+//         });
+//     });
+// }
+
+// getText('../content/first.txt')
+//     .then((result) => console.log(result))
+//     .catch((err) => console.log(err));
