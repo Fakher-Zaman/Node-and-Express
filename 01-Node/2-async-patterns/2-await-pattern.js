386f541688e85a4e6bcab0020fc3c2bba8e44899
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-07-20 16:59:35 +0500

    Async Patterns - Refactor To Async

--- 01-Node/2-async-patterns/2-await-pattern.js
+++ 01-Node/2-async-patterns/2-await-pattern.js
@@ -13,6 +13,19 @@ const getText = (path) => {
     });
 }
 
-getText('../content/first.txt')
-    .then((result) => console.log(result))
-    .catch((err) => console.log(err));
+// getText('../content/first.txt')
+//     .then((result) => console.log(result))
+//     .catch((err) => console.log(err));
+
+// ==>  async/await
+const start = async () => {
+    try {
+        const first = await getText('../content/first.txt');
+        const second = await getText('../content/second.txt');
+        console.log(first, second);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
