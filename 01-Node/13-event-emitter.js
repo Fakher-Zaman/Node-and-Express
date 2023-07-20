84f31789ba4d7df8fbae34e93ceb9ac415660615
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-07-20 20:14:56 +0500

    Events Emitter - Additional Info

--- 01-Node/13-event-emitter.js
+++ 01-Node/13-event-emitter.js
@@ -2,8 +2,12 @@ const EventEmitter = require('events');
 
 const customEmitter = new EventEmitter();
 
-customEmitter.on('response', () => {
-    console.log('data received');
+customEmitter.on('response', (name, id) => {
+    console.log(`data received user ${name} with id:${id}`);
 });
 
-customEmitter.emit('response');
+customEmitter.on('response', () => {
+    console.log(`some other logic here`);
+});
+
+customEmitter.emit('response', 'fakher', 22);
