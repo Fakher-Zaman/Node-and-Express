68cb22617b9784d446f1fe8dc925fa6dc886de8a
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-07-21 19:02:31 +0500

    Streams - Http Example

---
+++ 01-Node/17-http-stream.js
@@ -0,0 +1,14 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+    // const text = fs.readFileSync('./content/big.txt', 'utf8');
+    // res.end(text);
+    const fileStream = fs.createReadStream('./content/big.txt', 'utf-8');
+    fileStream.on('open', () => {
+        fileStream.pipe(res);
+    });
+    fileStream.on('error', (err) => {
+        res.end(err);
+    });
+}).listen(5000);
