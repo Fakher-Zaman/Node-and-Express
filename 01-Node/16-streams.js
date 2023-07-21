385f016f175bf639d1b85142ae4fb296870d4357
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-07-21 18:40:18 +0500

    Streams - Additional Info

--- 01-Node/16-streams.js
+++ 01-Node/16-streams.js
@@ -1,7 +1,20 @@
 const { createReadStream } = require('fs');
 
-const stream = createReadStream('./content/big.txt');
+const stream = createReadStream('./content/big.txt', {
+    highWaterMark: 90000,
+    encoding: 'utf8'
+});
+
+// default 64kb
+// last buffer - remainder
+// highWaterMark - control size
+// const stream = createReadStream('./content/big.txt', { highWaterMark: 90000 });
+// const stream = createReadStream('./content/big.txt', { encoding: 'utf8' });
 
 stream.on('data', (result) => {
     console.log(result);
+});
+
+stream.on('error', (err) => {
+    console.log(err);
 });
