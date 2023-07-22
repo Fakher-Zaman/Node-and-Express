0618ee281a1993ffefddc5f5e56129da79cbfd7f
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-07-22 18:28:02 +0500

    Http - Headers

--- 02-Express/final.js/01-http-basics.js
+++ 02-Express/final.js/01-http-basics.js
@@ -1,8 +1,9 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('user hits the server');
-    res.end('Welcome to our home page');
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.write('<h1>Hello World!</h1>');
+    res.end();
 });
 
 server.listen(5000);
