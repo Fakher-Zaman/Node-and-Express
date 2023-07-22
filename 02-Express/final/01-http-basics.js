3604f5a79af20fe29e5dc180b9121f91dec9ae45
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-07-22 20:35:01 +0500

    Http - Html File

--- 02-Express/final/01-http-basics.js
+++ 02-Express/final/01-http-basics.js
@@ -1,4 +1,8 @@
 const http = require('http');
+const { readFileSync } = require('fs');
+
+// get all files
+const homePage = readFileSync('../index.html');
 
 const server = http.createServer((req, res) => {
     // console.log(req.method);
@@ -6,7 +10,7 @@ const server = http.createServer((req, res) => {
     // home page
     if (url === '/') {
         res.writeHead(200, { 'content-type': 'text/html' });
-        res.write('<h1>home page</h1>');
+        res.write(homePage);
         res.end();
     }
     // about page 
