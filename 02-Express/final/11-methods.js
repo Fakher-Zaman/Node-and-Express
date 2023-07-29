cc442c92a642225c91ee14759e118446ad3bab02
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-07-29 18:29:08 +0500

    Methods - POST (Form Example)

--- 02-Express/final/11-methods.js
+++ 02-Express/final/11-methods.js
@@ -5,9 +5,22 @@ let { people } = require('../data');
 // static assets
 app.use(express.static('../methods-public'));
 
+// parse form data
+app.use(express.urlencoded({ extended: false }));
+
 // Method: GET
 app.get('/api/people', (req, res) => {
     res.status(200).json({ success: true, data: people });
+});
+
+app.post('/login', (req, res) => {
+    // console.log(req.body);
+    const { name } = req.body;
+    if (name) {
+        return res.status(200).send(`Welcome ${name}`);
+    }
+    // res.send('POST');
+    res.status(401).send('Please Provide Credentials');
 });
 
 const port = 5000;
