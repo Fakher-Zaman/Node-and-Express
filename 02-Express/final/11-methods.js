c6eed039610fee51e6d4855cd0ea3588c15519f1
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-07-29 17:06:39 +0500

    Methods - POST

--- 02-Express/final/11-methods.js
+++ 02-Express/final/11-methods.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 let { people } = require('../data');
 
+// static assets
+app.use(express.static('../methods-public'));
+
 // Method: GET
 app.get('/api/people', (req, res) => {
     res.status(200).json({ success: true, data: people });
