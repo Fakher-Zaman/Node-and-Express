2a8d1ae9a0d5cd694e00d3ab7c1ab9adec46d637
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-07-30 14:14:33 +0500

    Methods - PUT

--- 02-Express/final/11-methods.js
+++ 02-Express/final/11-methods.js
@@ -45,6 +45,24 @@ app.post('/api/postman/people', (req, res) => {
     res.status(201).json({ success: true, data: [...people, name] });
 });
 
+// Methods - PUT
+app.put('/api/people/:id', (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+
+    const person = people.find((person) => person.id === Number(id));
+    if (!person) {
+        return res.status(404).json({ success: false, msg: `No Person with id ${id}` });
+    }
+    const newPeople = people.map((person) => {
+        if (person.id === Number(id)) {
+            person.name = name;
+        }
+        return person;
+    });
+    res.status(200).json({ success: true, data: newPeople });
+});
+
 const port = 5000;
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
