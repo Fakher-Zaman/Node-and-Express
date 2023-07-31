d4f4dac7052e2469466ac963b49e46ff9af9260c
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-07-31 10:35:21 +0500

    Project Setup

--- 03-Task Manager/starter/app.js
+++ 03-Task Manager/starter/app.js
@@ -1,1 +1,15 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+
+app.get('/hello', (req, res) => {
+    res.send('Task Manager App');
+});
+
+// app.get('api/v1/task');          - get all the tasks
+// app.post('api/v1/task');         - create a new task
+// app.get('api/v1/task/:id');      - get a single task
+// app.patch('api/v1/task/:id');    - update a task
+// app.delete('api/v1/task/:id');   - delete a task
+
+const port = 3000;
+app.listen(port, console.log(`server is listening on port ${port}`));
--- 03-Task Manager/starter/public/edit-task.js
+++ 03-Task Manager/starter/public/edit-task.js
@@ -1,67 +1,67 @@
-const taskIDDOM = document.querySelector('.task-edit-id')
-const taskNameDOM = document.querySelector('.task-edit-name')
-const taskCompletedDOM = document.querySelector('.task-edit-completed')
-const editFormDOM = document.querySelector('.single-task-form')
-const editBtnDOM = document.querySelector('.task-edit-btn')
-const formAlertDOM = document.querySelector('.form-alert')
-const params = window.location.search
-const id = new URLSearchParams(params).get('id')
-let tempName
+const taskIDDOM = document.querySelector('.task-edit-id');
+const taskNameDOM = document.querySelector('.task-edit-name');
+const taskCompletedDOM = document.querySelector('.task-edit-completed');
+const editFormDOM = document.querySelector('.single-task-form');
+const editBtnDOM = document.querySelector('.task-edit-btn');
+const formAlertDOM = document.querySelector('.form-alert');
+const params = window.location.search;
+const id = new URLSearchParams(params).get('id');
+let tempName;
 
 const showTask = async () => {
   try {
     const {
       data: { task },
-    } = await axios.get(`/api/v1/tasks/${id}`)
-    const { _id: taskID, completed, name } = task
+    } = await axios.get(`/api/v1/tasks/${id}`);
+    const { _id: taskID, completed, name } = task;
 
-    taskIDDOM.textContent = taskID
-    taskNameDOM.value = name
-    tempName = name
+    taskIDDOM.textContent = taskID;
+    taskNameDOM.value = name;
+    tempName = name;
     if (completed) {
-      taskCompletedDOM.checked = true
+      taskCompletedDOM.checked = true;
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
-showTask()
+showTask();
 
 editFormDOM.addEventListener('submit', async (e) => {
-  editBtnDOM.textContent = 'Loading...'
-  e.preventDefault()
+  editBtnDOM.textContent = 'Loading...';
+  e.preventDefault();
   try {
-    const taskName = taskNameDOM.value
-    const taskCompleted = taskCompletedDOM.checked
+    const taskName = taskNameDOM.value;
+    const taskCompleted = taskCompletedDOM.checked;
 
     const {
       data: { task },
     } = await axios.patch(`/api/v1/tasks/${id}`, {
       name: taskName,
       completed: taskCompleted,
-    })
+    });
 
-    const { _id: taskID, completed, name } = task
+    const { _id: taskID, completed, name } = task;
 
-    taskIDDOM.textContent = taskID
-    taskNameDOM.value = name
-    tempName = name
+    taskIDDOM.textContent = taskID;
+    taskNameDOM.value = name;
+    tempName = name;
     if (completed) {
-      taskCompletedDOM.checked = true
+      taskCompletedDOM.checked = true;
     }
-    formAlertDOM.style.display = 'block'
-    formAlertDOM.textContent = `success, edited task`
-    formAlertDOM.classList.add('text-success')
+    formAlertDOM.style.display = 'block';
+    formAlertDOM.textContent = `success, edited task`;
+    formAlertDOM.classList.add('text-success');
   } catch (error) {
-    console.error(error)
-    taskNameDOM.value = tempName
-    formAlertDOM.style.display = 'block'
-    formAlertDOM.innerHTML = `error, please try again`
+    console.error(error);
+    taskNameDOM.value = tempName;
+    formAlertDOM.style.display = 'block';
+    formAlertDOM.innerHTML = `error, please try again`;
   }
-  editBtnDOM.textContent = 'Edit'
+  editBtnDOM.textContent = 'Edit';
   setTimeout(() => {
-    formAlertDOM.style.display = 'none'
-    formAlertDOM.classList.remove('text-success')
-  }, 3000)
-})
+    formAlertDOM.style.display = 'none';
+    formAlertDOM.classList.remove('text-success');
+  }, 3000);
+});
