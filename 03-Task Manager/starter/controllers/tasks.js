de6c0bbe1356fc1fc3e7ea8c1bead0f9554d9b25
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-08-01 12:51:34 +0500

    All Routes

--- 03-Task Manager/starter/controllers/tasks.js
+++ 03-Task Manager/starter/controllers/tasks.js
@@ -1,7 +1,27 @@
 const getAllTasks = (req, res) => {
-    res.send('All Items from the file');
+    res.send('Get All Tasks');
+};
+
+const createTask = (req, res) => {
+    res.send('Create Task');
+};
+
+const getTask = (req, res) => {
+    res.send('Get Single Task');
+};
+
+const updateTask = (req, res) => {
+    res.send('Update Task');
+};
+
+const deleteTask = (req, res) => {
+    res.send('Create Task');
 };
 
 module.exports = {
-    getAllTasks
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
 };
