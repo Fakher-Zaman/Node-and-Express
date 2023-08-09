9e548dfd82554b65a681b2da164673e0e3eae439
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-08-09 17:50:58 +0500

    ENV VARS

--- 03-Task-Manager/starter/app.js
+++ 03-Task-Manager/starter/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
+require('dotenv').config();
 
 // middleware
 app.use(express.json());
@@ -24,7 +25,7 @@ const port = 3000;
 
 const start = async () => {
     try {
-        await connectDB();
+        await connectDB(process.env.MONGO_URI);
         app.listen(port, console.log(`server is listening on port ${port}`));
     } catch (error) {
         console.log(error);
--- 03-Task-Manager/starter/db/connect.js
+++ 03-Task-Manager/starter/db/connect.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb://127.0.0.1:27017/nodejs-projects';
-// const connectionString = 'mongodb+srv://fakhar-zaman:noderoot@nodeexpressprojects.qfoyhbi.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority';
-
 const connectDB = (url) => {
-    return mongoose.connect(connectionString, {
+    return mongoose.connect(url, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true,
