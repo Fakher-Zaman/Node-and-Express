b229d5d7317950f49a6908f241f73e78f7797b08
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-08-10 17:54:42 +0500

    Get Single Task

--- 03-Task-Manager/starter/controllers/tasks.js
+++ 03-Task-Manager/starter/controllers/tasks.js
@@ -20,8 +20,18 @@ const createTask = async (req, res) => {
     }
 };
 
-const getTask = (req, res) => {
-    res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+    try {
+        const { id: taskID } = req.params;
+        const task = await Task.findOne({ _id: taskID });
+        if (!task) {
+            return res.status(404).json({ msg: `No task with id: ${taskID}` });
+        }
+        res.status(200).json({ task });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: error });
+    }
 };
 
 const updateTask = (req, res) => {
