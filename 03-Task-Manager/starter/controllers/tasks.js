1041ee6d79f8d6f68f429e7fc158abd35aa83688
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-08-10 17:38:15 +0500

    GetAllTasks Controller

--- 03-Task-Manager/starter/controllers/tasks.js
+++ 03-Task-Manager/starter/controllers/tasks.js
@@ -1,7 +1,13 @@
 const Task = require('../models/Task');
 
-const getAllTasks = (req, res) => {
-    res.send('Get All Tasks');
+const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find({});
+        res.status(200).json({ tasks });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: error });
+    }
 };
 
 const createTask = async (req, res) => {
