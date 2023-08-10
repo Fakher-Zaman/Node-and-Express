46aed8525168dbe255f2c41e8d9d0a6c273c5045
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-08-10 18:13:02 +0500

    Delete Task

--- 03-Task-Manager/starter/controllers/tasks.js
+++ 03-Task-Manager/starter/controllers/tasks.js
@@ -38,8 +38,18 @@ const updateTask = (req, res) => {
     res.send('Update Task');
 };
 
-const deleteTask = (req, res) => {
-    res.send('Delete Task');
+const deleteTask = async (req, res) => {
+    try {
+        const { id: taskID } = req.params;
+        const task = await Task.findByIdAndDelete({ _id: taskID });
+        if (!task) {
+            return res.status(404).json({ msg: `No task with id: ${taskID}` });
+        }
+        res.status(200).json({ task });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: error });
+    }
 };
 
 module.exports = {
