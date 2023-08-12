2ed0d3a79f12a46cdff38c34d21ba8ef0a43c0b0
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-08-12 16:36:29 +0500

    Custom Error Class

--- 03-Task-Manager/starter/controllers/tasks.js
+++ 03-Task-Manager/starter/controllers/tasks.js
@@ -1,5 +1,6 @@
 const Task = require('../models/Task');
 const asyncWrapper = require('../middleware/async');
+const { createCustomError } = require('../errors/customError');
 
 const getAllTasks = asyncWrapper(async (req, res) => {
     const tasks = await Task.find({});
@@ -13,11 +14,11 @@ const createTask = asyncWrapper(async (req, res) => {
     res.status(201).json({ task });
 });
 
-const getTask = asyncWrapper(async (req, res) => {
+const getTask = asyncWrapper(async (req, res, next) => {
     const { id: taskID } = req.params;
     const task = await Task.findOne({ _id: taskID });
     if (!task) {
-        return res.status(404).json({ msg: `No task with id: ${taskID}` });
+        return next(createCustomError(`No task with id: ${taskID}`, 404));
     }
     res.status(200).json({ task });
 });
@@ -26,7 +27,7 @@ const deleteTask = asyncWrapper(async (req, res) => {
     const { id: taskID } = req.params;
     const task = await Task.findByIdAndDelete({ _id: taskID });
     if (!task) {
-        return res.status(404).json({ msg: `No task with id: ${taskID}` });
+        return next(createCustomError(`No task with id: ${taskID}`, 404));
     }
     res.status(200).json({ task });
 });
@@ -39,7 +40,7 @@ const updateTask = asyncWrapper(async (req, res) => {
             runValidators: true,
         });
         if (!task) {
-            return res.status(404).json({ msg: `No task with id: ${taskID}` });
+            return next(createCustomError(`No task with id: ${taskID}`, 404));
         }
         res.status(200).json({ task });
     } catch (error) {
@@ -57,7 +58,7 @@ const editTask = asyncWrapper(async (req, res) => {
             overwrite: true,
         });
         if (!task) {
-            return res.status(404).json({ msg: `No task with id: ${taskID}` });
+            return next(createCustomError(`No task with id: ${taskID}`, 404));
         }
         res.status(200).json({ task });
     } catch (error) {
--- 03-Task-Manager/starter/middleware/errorHandler.js
+++ 03-Task-Manager/starter/middleware/errorHandler.js
@@ -1,5 +1,10 @@
+const customError = require('../errors/customError');
+
 const errorHandler = (err, req, res, next) => {
-    return res.status(500).json({ msg: `Something went wrong, try again later` });
+    if (err instanceof customError) {
+        return res.status(err.statusCode).json({ msg: err.message });
+    }
+    return res.status(500).json({ msg: 'Something went wrong, please try again later' });
 }
 
 module.exports = errorHandler;
