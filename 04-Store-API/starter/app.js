97358496a3c719dd9b5bd9a6610d4067be7c1bb3
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-08-17 15:56:51 +0500

    Create a basic Server Setup

--- 04-Store-API/starter/app.js
+++ 04-Store-API/starter/app.js
@@ -1,1 +1,35 @@
-console.log('04 Store API')
+require('dotenv').config();
+// async error handler
+
+const express = require('express');
+const app = express();
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get('/', (req, res) => {
+    res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
+});
+
+// products route
+
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+// connect to db
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        // await connectDB(process.env.MONGO_URI);
+        app.listen(port, console.log(`Server is listening on port ${port}...`));
+    } catch (error) {
+        console.log(error);
+    }
+}
+start();
