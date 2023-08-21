e002bcc8d644968a58bb3d518dc6bedca500d5e9
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-08-21 11:53:59 +0500

    Testing Data at Postman and Filter them by findOne()

--- 04-Store-API/starter/controllers/products.js
+++ 04-Store-API/starter/controllers/products.js
@@ -1,6 +1,11 @@
+const Product = require('../models/product');
+
 const getAllProductsStatic = async (req, res) => {
-    throw new Error('testing async errors');
-    res.status(200).json({ msg: 'products testing route' });
+    const products = await Product.find({
+        // featured: true,
+        name: 'vase table',
+    });
+    res.status(200).json({ products, nbHits: products.length });
 }
 const getAllProducts = async (req, res) => {
     res.status(200).json({ msg: 'products route' });
