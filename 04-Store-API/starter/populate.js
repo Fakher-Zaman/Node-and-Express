35afba5b907ea0cee14b44a7cec7dd6f5ec884d4
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-08-21 11:36:00 +0500

    Fetching products.json Data in MongoDB Atlas Successfully

--- 04-Store-API/starter/populate.js
+++ 04-Store-API/starter/populate.js
@@ -8,6 +8,8 @@ const jsonProducts = require('./products.json');
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI);
+        await Product.deleteMany();
+        await Product.create(jsonProducts);
         console.log('Success!');
     } catch (error) {
         console.log(error);
