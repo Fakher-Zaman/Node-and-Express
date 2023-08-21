d043faed62a2bbf10a13076350a728a1efffac3a
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-08-21 12:37:10 +0500

    Filter the data by find() but using Dynamic Approach

--- 04-Store-API/starter/controllers/products.js
+++ 04-Store-API/starter/controllers/products.js
@@ -8,7 +8,9 @@ const getAllProductsStatic = async (req, res) => {
     res.status(200).json({ products, nbHits: products.length });
 }
 const getAllProducts = async (req, res) => {
-    res.status(200).json({ msg: 'products route' });
+    // console.log(req.query);
+    const products = await Product.find(req.query);
+    res.status(200).json({ products, nbHits: products.length });
 }
 
 module.exports = {
