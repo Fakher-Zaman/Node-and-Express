cbd63a8c0363416614094c8a2b97a9c28a22a963
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-08-23 11:08:36 +0500

    Add queryObject in api controller to perform query

--- 04-Store-API/starter/controllers/products.js
+++ 04-Store-API/starter/controllers/products.js
@@ -2,14 +2,19 @@ const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
     const products = await Product.find({
-        // featured: true,
-        name: 'vase table',
+        page: '2',
     });
     res.status(200).json({ products, nbHits: products.length });
 }
+
 const getAllProducts = async (req, res) => {
-    // console.log(req.query);
-    const products = await Product.find(req.query);
+    const { featured } = req.query;
+    const queryObject = {};
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false;
+    }
+    console.log(queryObject);
+    const products = await Product.find(queryObject);
     res.status(200).json({ products, nbHits: products.length });
 }
 
