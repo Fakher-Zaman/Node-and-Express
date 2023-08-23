62e64d0f0c4503d26af260e5cfa31b0649bf7432
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-08-23 12:07:41 +0500

    Add Search with regex Functionality in api controller

--- 04-Store-API/starter/controllers/products.js
+++ 04-Store-API/starter/controllers/products.js
@@ -1,17 +1,24 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
+    const search = 'ab';
     const products = await Product.find({
-        page: '2',
+        name: { $regex: search, $options: 'i' },
     });
     res.status(200).json({ products, nbHits: products.length });
 }
 
 const getAllProducts = async (req, res) => {
-    const { featured } = req.query;
+    const { featured, company, name } = req.query;
     const queryObject = {};
     if (featured) {
         queryObject.featured = featured === 'true' ? true : false;
+    }
+    if (company) {
+        queryObject.company = company;
+    }
+    if (name) {
+        queryObject.name = { $regex: name, $options: 'i' };
     }
     console.log(queryObject);
     const products = await Product.find(queryObject);
