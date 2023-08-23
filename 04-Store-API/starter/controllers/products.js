f63d105396c869ad46a9575ccdee6e3a59fe083b
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-08-23 12:37:48 +0500

    Insert sort() functionality in getAllProducts

--- 04-Store-API/starter/controllers/products.js
+++ 04-Store-API/starter/controllers/products.js
@@ -6,7 +6,7 @@ const getAllProductsStatic = async (req, res) => {
 }
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name } = req.query;
+    const { featured, company, name, sort } = req.query;
     const queryObject = {};
     if (featured) {
         queryObject.featured = featured === 'true' ? true : false;
@@ -17,8 +17,15 @@ const getAllProducts = async (req, res) => {
     if (name) {
         queryObject.name = { $regex: name, $options: 'i' };
     }
-    console.log(queryObject);
-    const products = await Product.find(queryObject);
+    // console.log(queryObject);
+    let result = Product.find(queryObject);
+    if (sort) {
+        const sortList = sort.split(',').join(' ');
+        result = result.sort(sortList);
+    } else {
+        result = result.sort('createdAt');
+    }
+    const products = await result;
     res.status(200).json({ products, nbHits: products.length });
 }
 
