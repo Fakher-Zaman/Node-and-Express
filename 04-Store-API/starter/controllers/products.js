b88cd9fb4243d2e5580abed7a05c875ab240076f
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-08-24 11:40:51 +0500

    Implement page, limit and skip functionality in getAllProducts

--- 04-Store-API/starter/controllers/products.js
+++ 04-Store-API/starter/controllers/products.js
@@ -4,8 +4,8 @@ const getAllProductsStatic = async (req, res) => {
     const products = await Product.find({})
         .sort('name')
         .select('name price')
-        .limit(4)
-        .skip(2);
+    // .limit(4)
+    // .skip(2);
     res.status(200).json({ products, nbHits: products.length });
 }
 
@@ -33,6 +33,13 @@ const getAllProducts = async (req, res) => {
         const fieldsList = fields.split(',').join(' ');
         result = result.select(fieldsList);
     }
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit);
+    // 23
+    // 3 7 7 7 2
     const products = await result;
     res.status(200).json({ products, nbHits: products.length });
 }
