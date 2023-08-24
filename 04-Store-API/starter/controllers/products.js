742d2d4d230ea2e5b9e466cf2a34eaa0766a0b09
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-08-24 11:25:36 +0500

    Implement Limit and Skip functionality in static while manual testing

--- 04-Store-API/starter/controllers/products.js
+++ 04-Store-API/starter/controllers/products.js
@@ -1,7 +1,11 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({}).select('name price');
+    const products = await Product.find({})
+        .sort('name')
+        .select('name price')
+        .limit(4)
+        .skip(2);
     res.status(200).json({ products, nbHits: products.length });
 }
 
