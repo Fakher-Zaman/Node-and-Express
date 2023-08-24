81842dc29e7523beb7414ecb74821aef9ed9f140
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-08-24 14:29:32 +0500

    Converted values understood by the Mongoose to perform numericFilters task in products

--- 04-Store-API/starter/controllers/products.js
+++ 04-Store-API/starter/controllers/products.js
@@ -10,7 +10,7 @@ const getAllProductsStatic = async (req, res) => {
 }
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name, sort, fields } = req.query;
+    const { featured, company, name, sort, fields, numericFilters } = req.query;
     const queryObject = {};
     if (featured) {
         queryObject.featured = featured === 'true' ? true : false;
@@ -21,7 +21,22 @@ const getAllProducts = async (req, res) => {
     if (name) {
         queryObject.name = { $regex: name, $options: 'i' };
     }
-    // console.log(queryObject);
+    if (numericFilters) {
+        const operatorMap = {
+            '>': '$gt',
+            '>=': '$gte',
+            '=': '$eq',
+            '<': '$lt',
+            '<=': '$lte',
+        }
+        const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+        let filters = numericFilters.replace(
+            regEx,
+            (match) => `-${operatorMap[match]}-`
+        );
+        console.log(filters);
+    }
+    console.log(queryObject);
     let result = Product.find(queryObject);
     if (sort) {
         const sortList = sort.split(',').join(' ');
