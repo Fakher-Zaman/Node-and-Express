d722c4265c9d489145d3d786848e694177e74a9f
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-08-24 11:13:01 +0500

    Selects the specific fields using select() method

--- 04-Store-API/starter/controllers/products.js
+++ 04-Store-API/starter/controllers/products.js
@@ -1,12 +1,12 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({}).sort('-name price');
+    const products = await Product.find({}).select('name price');
     res.status(200).json({ products, nbHits: products.length });
 }
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name, sort } = req.query;
+    const { featured, company, name, sort, fields } = req.query;
     const queryObject = {};
     if (featured) {
         queryObject.featured = featured === 'true' ? true : false;
@@ -24,6 +24,10 @@ const getAllProducts = async (req, res) => {
         result = result.sort(sortList);
     } else {
         result = result.sort('createdAt');
+    }
+    if (fields) {
+        const fieldsList = fields.split(',').join(' ');
+        result = result.select(fieldsList);
     }
     const products = await result;
     res.status(200).json({ products, nbHits: products.length });
