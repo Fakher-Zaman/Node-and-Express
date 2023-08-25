00cf5528cea89a18444350b8bf5561a6232ffcd1
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-08-25 16:13:35 +0500

    Handle CustomAPIError with the help of middleware called error-handler

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,4 +1,22 @@
+// check username, password in post(login) request
+// if exist create new JWT
+// send back to front end
+
+// setup authentication so only the request with JWT can access dashboard
+
+const CustomAPIError = require('../errors/custom-error');
+
 const login = async (req, res) => {
+    const { username, password } = req.body;
+    // When user not provided username or password, we need to throw an error: by using these 3 methods:
+    // 1. mongoose validation
+    // 2. Joi - package name
+    // 3. check in the controller
+
+    if (!username || !password) {
+        throw new CustomAPIError('Please provide email and password', 400);
+    }
+    // console.log(username, password);
     res.send('Fake Login/Register/Signup Route');
 };
 
