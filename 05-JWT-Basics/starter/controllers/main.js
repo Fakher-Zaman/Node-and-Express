0417efab1131e39f6ae6ce7a39d21e5e15392b50
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-08-26 20:42:49 +0500

    Set authHeader and check the accuracy

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -30,6 +30,16 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
+    // console.log(req.headers);
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        throw new CustomAPIError('No token provided', 401);
+    }
+
+    const token = authHeader.split(' ')[1];
+    // console.log(token);
+
     const luckyNumber = Math.floor(Math.random() * 100);
     res.status(200).json({
         msg: "Hello, John Doe",
