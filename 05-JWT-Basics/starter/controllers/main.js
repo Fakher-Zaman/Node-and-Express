9f8a66092e385b01998aef39c6151fb46e519bdb
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-08-27 11:40:36 +0500

    Connect the controller and middleware together to perform a specific task

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -30,32 +30,12 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-    // console.log(req.headers);
-    const authHeader = req.headers.authorization;
-
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        throw new CustomAPIError('No token provided', 401);
-    }
-
-    const token = authHeader.split(' ')[1];
-    // console.log(token);
-
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        // console.log(decoded);
-        const luckyNumber = Math.floor(Math.random() * 100);
-        res.status(200).json({
-            msg: `Hello, ${decoded.username}`,
-            secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
-        });
-        // const { id, username } = decoded;
-        // console.log(id, username);
-        // req.user = { id, username };
-        // next();
-    } catch (error) {
-        throw new CustomAPIError('Not authorized to access this route', 401);
-    }
-
+    console.log(req.user);
+    const luckyNumber = Math.floor(Math.random() * 100);
+    res.status(200).json({
+        msg: `Hello, ${req.user.username}`,
+        secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+    });
 }
 
 module.exports = {
--- 05-JWT-Basics/starter/middleware/auth.js
+++ 05-JWT-Basics/starter/middleware/auth.js
@@ -1,6 +1,20 @@
+const jwt = require('jsonwebtoken');
+const CustomAPIError = require('../errors/custom-error');
+
 const authenticationMiddleware = (req, res, next) => {
-    console.log(req.headers.authorization);
-    next();
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        throw new CustomAPIError('No token provided', 401);
+    }
+    const token = authHeader.split(' ')[1];
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const { id, username } = decoded;
+        req.user = { id, username };
+        next();
+    } catch (error) {
+        throw new CustomAPIError('Not authorized to access this route', 401);
+    }
 }
 
 module.exports = authenticationMiddleware;
