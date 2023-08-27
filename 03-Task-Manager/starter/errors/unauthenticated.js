99b5d51d64cf557377b43de8667a369936296040
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-08-27 12:12:25 +0500

    Refactoring of status codes by using http-status-codes for more consistency in code

--- 03-Task-Manager/starter/errors/unauthenticated.js
+++ 03-Task-Manager/starter/errors/unauthenticated.js
@@ -1,9 +1,10 @@
 const CustomAPIError = require('./custom-error');
+const { StatusCodes } = require('http-status-codes');
 
 class UnauthenticatedError extends CustomAPIError {
     constructor(message) {
         super(message);
-        this.statusCode = 401;
+        this.statusCode = StatusCodes.UNAUTHORIZED;
     }
 }
 
