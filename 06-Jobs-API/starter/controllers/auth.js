ce9a1b1fc6cf7e9e187d27d971f4dd920e139bfa
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-08-28 14:47:01 +0500

    Working with register in auth.js to control the data

--- 06-Jobs-API/starter/controllers/auth.js
+++ 06-Jobs-API/starter/controllers/auth.js
@@ -1,5 +1,9 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+
 const register = async (req, res) => {
-    res.send('register user');
+    const user = await User.create({ ...req.body });
+    res.status(StatusCodes.CREATED).json({ user });
 }
 
 const login = async (req, res) => {
