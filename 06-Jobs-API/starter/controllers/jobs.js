dc22d1a908a595a9b39ad020a081b481902eaa41
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-08-28 12:58:53 +0500

    Create a basic controller structure

---
+++ 06-Jobs-API/starter/controllers/jobs.js
@@ -0,0 +1,27 @@
+const getAllJobs = async (req, res) => {
+    res.send('get all jobs');
+}
+
+const getJob = async (req, res) => {
+    res.send('get single job');
+}
+
+const createJob = async (req, res) => {
+    res.send('create job');
+}
+
+const updateJob = async (req, res) => {
+    res.send('update job');
+}
+
+const deleteJob = async (req, res) => {
+    res.send('delete job');
+}
+
+module.exports = {
+    getAllJobs,
+    getJob,
+    createJob,
+    updateJob,
+    deleteJob
+}
