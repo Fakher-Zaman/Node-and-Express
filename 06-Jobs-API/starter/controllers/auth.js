db18a4e74415d65b2ea1ac9f427a3626922281f6
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-08-29 11:35:42 +0500

    Authenticate the data in another way by using Mongoose middleware Schema

--- 06-Jobs-API/starter/controllers/auth.js
+++ 06-Jobs-API/starter/controllers/auth.js
@@ -1,16 +1,8 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
-const bcrypt = require('bcryptjs');
 
 const register = async (req, res) => {
-    const { name, email, password } = req.body;
-
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    const tempUser = { name, email, password: hashedPassword };
-
-    const user = await User.create({ ...tempUser });
+    const user = await User.create({ ...req.body });
     res.status(StatusCodes.CREATED).json({ user });
 }
 
--- 06-Jobs-API/starter/models/User.js
+++ 06-Jobs-API/starter/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -24,4 +25,8 @@ const userSchema = new mongoose.Schema({
     },
 });
 
+userSchema.pre('save', async function () {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
 module.exports = mongoose.model('users', userSchema);
