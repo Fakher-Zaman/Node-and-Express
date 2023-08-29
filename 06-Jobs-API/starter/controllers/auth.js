09a63e7a6711af2c7cb22fb7a46a591f85492f15
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-08-29 11:49:55 +0500

    Make application more authenticate use the JWT token

--- 06-Jobs-API/starter/controllers/auth.js
+++ 06-Jobs-API/starter/controllers/auth.js
@@ -1,9 +1,11 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
+const jwt = require('jsonwebtoken');
 
 const register = async (req, res) => {
     const user = await User.create({ ...req.body });
-    res.status(StatusCodes.CREATED).json({ user });
+    const token = jwt.sign({ userId: user._id, name: user.name }, 'jwtSecret', { expiresIn: '30d' });
+    res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 }
 
 const login = async (req, res) => {
