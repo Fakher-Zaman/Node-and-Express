4bb4d860202a92a8c8bdffc1083e67a145aebc4a
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-08-30 12:59:44 +0500

    Create Login functionality in auth.js controller

--- 06-Jobs-API/starter/controllers/auth.js
+++ 06-Jobs-API/starter/controllers/auth.js
@@ -1,6 +1,7 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const jwt = require('jsonwebtoken');
+const { BadRequestError, UnauthenticatedError } = require('../errors');
 
 const register = async (req, res) => {
     const user = await User.create({ ...req.body });
@@ -9,7 +10,17 @@ const register = async (req, res) => {
 }
 
 const login = async (req, res) => {
-    res.send('login user');
+    const { email, password } = req.body;
+    if (!email || !password) {
+        throw new BadRequestError('Please provide email and password')
+    }
+    const user = await User.findOne({ email });
+    // compare password
+    if (!user) {
+        throw new UnauthenticatedError('Invalid Credentials');
+    }
+    const token = user.createJWT();
+    res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 }
 
 module.exports = {
