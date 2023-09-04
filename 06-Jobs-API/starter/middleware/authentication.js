fca17965ebeb3fef5ac3aac520c397267c72d3ca
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-09-04 11:55:57 +0500

    Finalize the Jobs API Project

--- 06-Jobs-API/starter/middleware/authentication.js
+++ 06-Jobs-API/starter/middleware/authentication.js
@@ -14,6 +14,10 @@ const auth = async (req, res, next) => {
     try {
         const payload = jwt.verify(token, process.env.JWT_SECRET);
         // attach user to request object
+
+        const user = user.findById(payload.userId).select('-password');
+        req.user = user;
+
         req.user = { userId: payload.userId, name: payload.name };
         next();
     } catch (error) {
